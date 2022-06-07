e444580d62cc1ee23e5e61ba254c7a0f41774c6e
Алексей <t1meal@yandex.ru>
2022-06-07 12:25:50 +0300

    init commit

---
+++ src/main/java/ru/gb/market/SpringMarketApplication.java
@@ -0,0 +1,13 @@
+package ru.gb.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMarketApplication.class, args);
+	}
+
+}
